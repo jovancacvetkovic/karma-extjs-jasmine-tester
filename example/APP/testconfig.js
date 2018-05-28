eac61bb02f9f23a27139fa511524cde45e22feaf
jcvetkovic <jovanca.cvetkovic@gmail.com>
2018-05-28 13:08:36 +0200

    removed example

--- example/APP/testconfig.js
+++
@@ -1,9 +0,0 @@
-/*global window*/
-
-Ext.Loader.setConfig({
-    enabled: true,
-    paths: {
-        'APP': 'http://' + window.location.host + '/base/APP/src'        
-    },
-    disableCaching: true
-});
--- example/README.md
+++
@@ -1,9 +0,0 @@
-# Example
-
-Do the following to run tests with coverage
-
-```bash
-$ npm install
-$ gulp coverage
-```
-
